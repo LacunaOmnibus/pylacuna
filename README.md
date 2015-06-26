279c037fbb511c92a7adb53fbf18cfc0ced7bf65
Michael Aherne <miketwo@gmail.com>
2015-06-25 17:33:06 -0700

    starting to split things up into appropriate directories

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pylacuna
 Python api for Lacuna Expanse game
+
+I'm trying to accomplish two things here.
+
+1) Produce generic python libraries for interacting with the Lacuna Expanse game.
+
+2) Use those libraries with some AI to create an automated bot to play the game.
+
+
+DIRECTORY STRUCTURE
+.
+├── bin            # scripts
+├── pylacuna       # libraries
+└── var            # generated files
---
+++ pylacuna/errors.py
@@ -0,0 +1,25 @@
+
+# TBD
+
+# 1000 Name not available.
+# 1001 Invalid password.
+# 1002 Object does not exist.
+# 1003 Too much information.
+# 1004 Password incorrect.
+# 1005 Contains invalid characters.
+# 1006 Session expired.
+# 1007 Overspend.
+# 1008 Underspend.
+# 1009 Invalid range.
+# 1010 Insufficient privileges.
+# 1011 Not enough resources in storage.
+# 1012 Not enough resources in production.
+# 1013 Missing prerequisites.
+# 1014 Captcha not valid.
+# 1015 Restricted for sitter logins.
+# 1016 Needs to solve a captcha.
+# 1017 Pending Parliamentary Vote
+# 1018 Already Voted
+# 1100 Empire not founded.
+# 1101 Empire not founded, and you tried to create it, but had the wrong password.
+# 1200 Game Over.
